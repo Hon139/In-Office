47f914b42966698d52e49e2a787930ba6caab5fc
cocacolachicken <tylerjamesgu@gmail.com>
2025-10-04 19:33:06 -0400

    mfwakemf

--- src/index.js
+++ src/index.js
@@ -4,28 +4,12 @@ import { resources } from "./resources.js";
 import RoomScreen from "./js/stage/room.js";
 
 
-const JitsiState = {
-  active: false,
-  currentRoom: "",
-  activateMeeting: () => {
-    console.log("activating meeting...")
-    const api = new JitsiMeetExternalAPI("meet.jit.si/teaaaaaam", {
-      roomName: "Hackthevalleytestroom",
-      width: 300,
-      height: 300,
-      parentNode: document.querySelector("#jitsi-meeting")
-    })
-  }
-
-}
-
-
 
 me.device.onReady(() => {
   me.video.init(960, 540, { parent: "screen", scale: "auto", renderer: me.video.AUTO });
   me.loader.preload(resources, () => {
     me.state.set(me.state.PLAY, new PlayScreen());
     me.state.set(me.state.ROOM, new RoomScreen());
-    me.state.change(me.state.PLAY);
+    me.state.change(me.state.ROOM);
   });
 });
