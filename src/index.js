c91cdbd8bfd614c0dbc094464e26e3aa628bd0de
TaoEvan <tao.evan.pi@gmail.com>
2025-10-05 04:45:34 -0400

    fixed viewport and jitsi

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import { resources } from './resources.js';
 import RoomScreen from './js/stage/room.js';
 
 me.device.onReady(() => {
-  me.video.init(960, 540, { parent: 'screen', scale: 'fit', renderer: me.video.AUTO });
+  me.video.init(960, 540, { parent: 'screen', scaleMethod: 'flex', renderer: me.video.AUTO });
+  
   me.loader.preload(resources, () => {
     me.state.set(me.state.PLAY, new PlayScreen());
     me.state.set(me.state.ROOM, new RoomScreen());
-    me.state.change(me.state.PLAY);
+    me.state.change(me.state.ROOM);
   });
 });
