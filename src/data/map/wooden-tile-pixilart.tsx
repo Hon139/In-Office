8513e36889406fd1c4b9e8aefda78e6f8b54e13e
Carol Wang <Carol.Wang@pinkbyte.com>
2025-10-05 11:45:23 -0400

    Patching bugs

--- src/data/map/wooden-tile-pixilart.tsx
+++ src/data/map/wooden-tile-pixilart.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="wooden-tile-pixilart" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../Downloads/pixelset/wooden-tile-pixilart.png" width="32" height="32"/>
+ <image source="wooden-tile-pixilart.png" width="32" height="32"/>
 </tileset>
